ffd22a0788ce19a3ae4d56424570e1f073123a74
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-06-28 12:02:04 +0530

    Update Authordata.js

--- src/model/Authordata.js
+++ src/model/Authordata.js
@@ -1,10 +1,10 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/library');
-// const Schema = mongoose.Schema;
-// const AuthorSchema = new Schema({
-//       title: String,
-// description: String,
-//       image: String
-// });
-// var Authordata = mongoose.model('authordata',AuthorSchema);
-// module.exports = Authordata;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/library');
+const Schema = mongoose.Schema;
+const AuthorSchema = new Schema({
+      title: String,
+description: String,
+      image: String
+});
+var Authordata = mongoose.model('authordata',AuthorSchema);
+module.exports = Authordata;
