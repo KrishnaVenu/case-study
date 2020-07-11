89c918296ae01c990d7cc8a170c13b63c8e184e5
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-07-11 22:50:26 +0530

    Add files via upload

---
+++ src/routes/updateBookRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const updateBookRouter = express.Router();
+const Bookdata = require('../model/Bookdata');
+function router(nav2){
+updateBookRouter.get('/:id',function(req,res){
+   const id =req.params.id;
+   Bookdata.findOne({_id:id})
+   .then(function(book){
+    res.render('updateBook',
+    {
+       nav2,
+       title:"V.K.Library-Books Updation",
+       book
+    });
+   })
+});
+ updateBookRouter.post('/post',function(req,res){
+    var id = req.body._id;
+   // console.log(id);
+   var item = {    
+      title:  req.body.title,
+description: req.body.description,
+      image:  req.body.image
+}
+    // var book = Bookdata(item);
+            Bookdata.findByIdAndUpdate(id,item,function(err,result){
+               if(result){
+                  // res.render('book'),
+                  // {
+                  //    nav2,
+                  //    title:"V.K.Library-Books"
+                  // }
+                  res.redirect("books[i]");
+               }
+     })
+               
+  });
+            return updateBookRouter;
+}
+        module.exports = router;
